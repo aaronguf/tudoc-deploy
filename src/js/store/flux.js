053a5a19bf6b17f0f8356cdf1859399458c7cc7d
Aaron Guzman <aaronguf.93@hotmail.com>
2024-01-30 21:47:00 -0600

    update fetch url

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -19,7 +19,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 						password: password
 					};
 
-					const response = await fetch("http://127.0.0.1:5000/users", {
+					const response = await fetch("https://test-deploy-back-on-render-7mfe.onrender.com/users", {
 						method: "POST",
 						headers: {
 							"Content-Type": "application/json"
